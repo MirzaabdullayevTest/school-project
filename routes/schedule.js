3a1c4bda7d218a7c91db52a6e0f2b10318a91985
Mirzaabdullayev Javlonbek <mirzaabdullayevtest@gmail.com>
2023-12-27 19:26:39 +0500

    [+] schedule table get router has been created

--- routes/schedule.js
+++ routes/schedule.js
@@ -35,4 +35,62 @@ router.post('/create', auth, async (req, res) => {
     res.status(201).send('Successfully created')
 })
 
+router.get('/get/:id', auth, async (req, res) => {
+    const schedule = await Schedule.findOne({schoolId: req.params.id})
+
+    if(!schedule){
+        res.send('Schedule not found in this school')
+        return
+    }
+
+    const table = generateTable(schedule)
+
+    res.send(table)
+})
+
+function generateTable(schedule){
+    const table = []
+
+    const schoolOverTimeInMinutes = convertToMinutes(schedule.endTime)
+    let startTimeInMinutes = convertToMinutes(schedule.startTime)
+    let number = 1
+
+    while(schoolOverTimeInMinutes > startTimeInMinutes){
+        let startTime = convertToHHMM(startTimeInMinutes)
+        let endTimeInMinutes = startTimeInMinutes + parseInt(schedule.duration)
+        let endTime = convertToHHMM(endTimeInMinutes)
+        let breakTime = parseInt(schedule.breakTime)
+
+        if(number === parseInt(schedule.bigBreakAfterLesson)){
+            breakTime = parseInt(schedule.bigBreakTime)
+        }
+
+        let lesson = {
+            number,
+            startTime,
+            endTime,
+            breakTime
+        }
+
+        table.push(lesson)
+
+        startTimeInMinutes = endTimeInMinutes + breakTime
+        number++
+        breakTime = parseInt(schedule.breakTime)
+    }
+
+    return table
+}
+
+function convertToMinutes(time){
+    const [hours, minutes] = time.split(':');
+    return parseInt(hours) * 60 + parseInt(minutes);
+}
+
+function convertToHHMM(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${String(hours).padStart(2, '0')}:${String(remainingMinutes).padStart(2, '0')}`;
+}
+
 module.exports = router
