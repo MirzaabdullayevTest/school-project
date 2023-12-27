4fab6144120ebf2e554e02dd700ee41a19701c0d
Mirzaabdullayev Javlonbek <mirzaabdullayevtest@gmail.com>
2023-12-27 18:23:46 +0500

    [+] schedule create router has been created

---
+++ routes/schedule.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const Schedule = require('../models/Schedule')
+const School = require('../models/School')
+const auth = require('../middleware/auth')
+
+router.post('/create', auth, async (req, res) => {
+    const {schoolId, startTime, duration, breakTime, bigBreakTime, bigBreakAfterLesson, endTime} = req.body
+    const school = await School.findById(schoolId)
+
+    if(!req.admin.isHeadAdmin && school.userId !== req.admin._id){
+        res.send('You are not head admin or school user')
+        return
+    }
+
+    const schedule = new Schedule({
+        schoolId,
+        startTime,
+        duration,
+        breakTime,
+        bigBreakTime,
+        bigBreakAfterLesson,
+        endTime,
+    })
+
+    await schedule.save()
+
+    res.status(201).send('Successfully created')
+})
+
+module.exports = router
