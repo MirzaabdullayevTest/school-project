8f587960090e48ce761f565e8540585c3e4d220a
Mirzaabdullayev Javlonbek <mirzaabdullayevtest@gmail.com>
2023-12-28 17:07:27 +0500

    [+] full table with schedule times has been created

--- routes/table.js
+++ routes/table.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const Table = require('../models/Table')
+const Class = require('../models/Class')
+const Schedule = require('../models/Schedule')
 const auth = require('../middleware/auth')
+const generateTable = require('../helper/generateTable')
 
 router.post('/create', auth, async (req, res) => {
     const {classId, lessonNumber, subject, teacher, room, dayId} = req.body
@@ -23,9 +26,39 @@ router.get('/get/:dayId/:classId', auth, async (req, res) => {
         return
     }
 
-    res.status(200).send(table)
+    const clas = await Class.findById(req.params.classId)
+    const schedule = await Schedule.findOne({schoolId: clas.schoolId})
+
+    const scheduleTimes = generateTable(schedule)
+
+    const fullTable = getFullTable(table, scheduleTimes)
+
+    res.status(200).send(fullTable)
 })
 
+function getFullTable(table, times){
+    let tableLength = table.length
+    let fullTable = []
+    let i = 1
 
+    while(tableLength >= i){
+        let lesson = table[i - 1]
+        let time = times[i - 1]
+
+        fullTable.push({
+            classId: lesson.classId,
+            dayId: lesson.dayId,
+            lessonNumber: lesson.lessonNumber,
+            subject: lesson.subject,
+            teacher: lesson.teacher,
+            room: lesson.room,
+            startTime: time.startTime,
+            endTime: time.endTime
+        })
+        i++
+    }
+
+    return fullTable
+}
 
 module.exports = router
