3d8967254bec33922059c927fcadc72653d1678c
Mirzaabdullayev Javlonbek <mirzaabdullayevtest@gmail.com>
2024-01-06 18:34:05 +0500

    [+] error handler has been added

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const port = 8080
 const host = 'localhost'
 
 
-require('./helper/db')()
-
 app.set('trust proxy', true);
 
 app.use(express.json());
@@ -19,13 +17,17 @@ app.use('/api/admin', require('./routes/admin'))
 app.use('/api/user', require('./routes/user'))
 app.use('/api/client-user', require('./routes/client-user'))
 
-app.use(auth)
-app.use('/api/school', require('./routes/school'))
-app.use('/api/schedule', require('./routes/schedule'))
-app.use('/api/class', require('./routes/class'))
-app.use('/api/table', require('./routes/table'))
-app.use('/api/weekdays', require('./routes/week-days'))
+app.use('/api/school', auth, require('./routes/school'))
+app.use('/api/schedule', auth, require('./routes/schedule'))
+app.use('/api/class', auth, require('./routes/class'))
+app.use('/api/table', auth, require('./routes/table'))
+app.use('/api/weekdays', auth, require('./routes/week-days'))
 
-app.listen(port, host, ()=>{
+app.use(function (req, res, next) {
+   return res.send(404)
+});
+
+app.listen(port, host, async () => {
+    await require('./helper/db')()
     console.log(`App listening on http://${host}:${port} || ${new Date()}`)
 })
--- client/app.js
+++ client/app.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const auth = require('./middleware/auth')
 
+
+app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -10,15 +13,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(require('./middleware/logger'))
 app.use('/auth', require('./routes/auth'));
 
-app.use(require('./middleware/auth'))
+app.use('/schedule', auth, require('./routes/schedule'))
 
-app.use('/schedule', require('./routes/schedule'))
+app.use('/class', auth, require('./routes/class'))
 
-app.use('/class', require('./routes/class'))
+app.use('/weekdays', auth, require('./routes/weekdays'))
 
-app.use('/weekdays', require('./routes/weekdays'))
+app.use('/table', auth, require('./routes/table'))
 
-app.use('/table', require('./routes/table'))
+app.use(function (req, res, next) {
+   return res.send(404)
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
